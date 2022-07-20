03ab0a523aa99d74ecd79e5b50725b7b367ab67c
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2022-07-20 13:25:23 +0200

    typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header.js";
 import Meals from "./components/Meals/Meals.js";
 import Cart from "./components/Cart/Cart.js";
-import CartProvider from "./store/CartProvide.js";
+import CartProvider from "./store/CartProvider.js";
 
 function App() {
     const [cartIsShown, setCartIsShown] = useState(false);
