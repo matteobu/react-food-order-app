2902d3893dc5ce2f33fa8b7188954d509d44172d
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2022-10-10 17:02:31 +0200

    passing id + bug fix

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -7,7 +7,7 @@ const MealItemForm = (props) => {
             <Input
                 label="Amount"
                 input={{
-                    id: "Amount",
+                    id: "amount_" + props.id,
                     type: "number ",
                     min: "1",
                     max: "5",
